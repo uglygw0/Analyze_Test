8c4040767923ead736c354233948adc245b52b06
uglygw0 <uglygw0@gmail.com>
2026-05-01 22:03:50 +0900

    Auto-save

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef } from 'react';
+import React, { useState, useMemo, useRef, useEffect } from 'react';
 import Papa from 'papaparse';
 import * as XLSX from 'xlsx';
 import { 
